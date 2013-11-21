7e5a8b705ba05d74d1aa9297d5d92eded26926ea
lili <lili00681@gmail.com>
2013-11-21 05:13:57 -0800

    一个不错的项目框架

---
+++ src/project/src/add/add.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  add.c
+ *
+ *    Description:  add 函数，就是个例子
+ *
+ *        Version:  1.0
+ *        Created:  2013年07月26日 00时04分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include "add.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
---
+++ src/project/src/main/main.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  主程序
+ *
+ *        Version:  1.0
+ *        Created:  2013年07月26日 00时03分28秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "add.h"
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", add(1, 1));
+
+	return EXIT_SUCCESS;
+}
+
