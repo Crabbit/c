12d571ad5c1280eea62420570ad3f9e8ef82df45
lili <lili00681@gmail.com>
2014-03-18 00:23:49 -0800

    git add -A

--- src/system/ctrl_mem.c
+++ src/system/ctrl_mem.c
@@ -26,10 +26,12 @@
 #define MIN_SLICE 10
 #define DEFAULT_MEM_SIZE 1024
 #define DEFAULT_MEM_START 0
+
 // 内存分配算法
 #define MA_FF 1
 #define MA_BF 2
 #define MA_WF 3
+
 int mem_size = DEFAULT_MEM_SIZE;
 int ma_algorithm = MA_FF;
 static int pid = 0;
